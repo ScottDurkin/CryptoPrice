24d513f0efd1c07637525945bdb373c1691b1a2f
Scott Durkin <scottdurkin97@gmail.com>
2021-11-25 23:51:54 +0000

    Pack updates

--- TestingProgram/Program.cs
+++ TestingProgram/Program.cs
@@ -1,46 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
 using CryptoPrice;
 using CryptoPrice.Definitions;
-namespace TestingProgram
+namespace TestNugets
 {
     class Program
     {
         static void Main(string[] args)
         {
-            CryptoCore cp = new CryptoCore("");
+            //Initalize the library by passing your API key into it.
+            CryptoCore _CryptoPrice = new CryptoCore("");
 
-            cp.SetCurrency = Fiat.USN;
-            //cp.SetCurrency = "XAU";
-            String test = "";
-            Console.WriteLine(test);
+            //Set the currency for the price return - Default is USD.
+            _CryptoPrice.SetCurrency = Fiat.EUR;
 
+            //Get the current price of Bitcoin
+            double BitcoinPrice = _CryptoPrice.GetCryptoPrice("BTC");
 
-
-
-            #region create blocks of code
-            //List<String> Finished = new List<string>();
-
-            //foreach (String line in System.IO.File.ReadLines(@"C:\Source\CryptoPrice\TestingProgram\Currencies.txt"))
-            //{
-            //    if (Finished.Contains(line) == false)
-            //    {
-            //        StringBuilder sb = new StringBuilder();
-            //        sb.Append(String.Format("public static String {0}", line));
-            //        sb.Append("{");
-            //        String code = "get { return \"%%\"; }";
-            //        code = code.Replace("%%", line);
-            //        sb.Append(code);
-            //        sb.Append("}");
-
-            //        Finished.Add(line);
-            //        Console.WriteLine(sb);
-            //    }
-            //}
-            #endregion
+            //Output to console
+            Console.WriteLine(BitcoinPrice);
         }
     }
 }

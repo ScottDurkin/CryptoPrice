f8a9f2fa91d1ffd3515d95c9ceb2e4f4c71c240e
Scott Durkin <scottdurkin97@gmail.com>
2021-11-25 21:32:23 +0000

    Add project files.

---
+++ CryptoPrice/ApiPrice.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoPrice
+{
+    internal class _1d
+    {
+        public string volume { get; set; }
+        public string price_change { get; set; }
+        public string price_change_pct { get; set; }
+        public string volume_change { get; set; }
+        public string volume_change_pct { get; set; }
+        public string market_cap_change { get; set; }
+        public string market_cap_change_pct { get; set; }
+    }
+
+    internal class _7d
+    {
+        public string volume { get; set; }
+        public string price_change { get; set; }
+        public string price_change_pct { get; set; }
+        public string volume_change { get; set; }
+        public string volume_change_pct { get; set; }
+        public string market_cap_change { get; set; }
+        public string market_cap_change_pct { get; set; }
+    }
+
+    internal class _30d
+    {
+        public string volume { get; set; }
+        public string price_change { get; set; }
+        public string price_change_pct { get; set; }
+        public string volume_change { get; set; }
+        public string volume_change_pct { get; set; }
+        public string market_cap_change { get; set; }
+        public string market_cap_change_pct { get; set; }
+    }
+
+    internal class _365d
+    {
+        public string volume { get; set; }
+        public string price_change { get; set; }
+        public string price_change_pct { get; set; }
+        public string volume_change { get; set; }
+        public string volume_change_pct { get; set; }
+        public string market_cap_change { get; set; }
+        public string market_cap_change_pct { get; set; }
+    }
+
+    internal class Ytd
+    {
+        public string volume { get; set; }
+        public string price_change { get; set; }
+        public string price_change_pct { get; set; }
+        public string volume_change { get; set; }
+        public string volume_change_pct { get; set; }
+        public string market_cap_change { get; set; }
+        public string market_cap_change_pct { get; set; }
+    }
+
+    internal class Root
+    {
+        public string id { get; set; }
+        public string currency { get; set; }
+        public string symbol { get; set; }
+        public string name { get; set; }
+        public string logo_url { get; set; }
+        public string status { get; set; }
+        public string price { get; set; }
+        public DateTime price_date { get; set; }
+        public DateTime price_timestamp { get; set; }
+        public string circulating_supply { get; set; }
+        public string max_supply { get; set; }
+        public string market_cap { get; set; }
+        public string market_cap_dominance { get; set; }
+        public string num_exchanges { get; set; }
+        public string num_pairs { get; set; }
+        public string num_pairs_unmapped { get; set; }
+        public DateTime first_candle { get; set; }
+        public DateTime first_trade { get; set; }
+        public DateTime first_order_book { get; set; }
+        public string rank { get; set; }
+        public string rank_delta { get; set; }
+        public string high { get; set; }
+        public DateTime high_timestamp { get; set; }
+        public _1d _1d { get; set; }
+        public _7d _7d { get; set; }
+        public _30d _30d { get; set; }
+        public _365d _365d { get; set; }
+        public Ytd ytd { get; set; }
+    }
+
+
+}
---
+++ CryptoPrice/CryptoPrice.cs
@@ -0,0 +1,39 @@
+﻿using CryptoPrice;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace CryptoPrice
+{
+    public class CryptoPrice
+    {
+        protected WebClient _webClient;
+        protected String API_URL = "https://api.nomics.com/v1/currencies/ticker?key={0}&ids={1}&convert=USD&per-page=100&page=1";
+        protected String API_KEY = "7c9761d7177d7a673dc03035b680b08acc9023fc";
+        protected String BACKUP_API_KEY = "7c9761d7177d7a673dc03035b680b08acc9023fc";
+
+        public CryptoPrice(String ApiKey)
+        {
+            API_KEY = ApiKey;
+            _webClient = new WebClient();
+
+            if (API_KEY == "" || API_KEY == null)
+                API_KEY = BACKUP_API_KEY;
+        }
+
+        public double GetCryptoPrice(String Symbol = "BTC")
+        {
+            return GetCoinPrice(Symbol);
+        }
+
+        protected double GetCoinPrice(String Symbol)
+        {
+            String request = String.Format(API_URL, API_KEY, Symbol);
+            String GetLatest = _webClient.DownloadString(request);
+
+            List<Root> listRoot = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Root>>(GetLatest);
+
+            return Convert.ToDouble(listRoot[0].price);
+        }
+    }
+}

c74621ca150648902f1106222a93112d6e39e40d
Scott Durkin <scottdurkin97@gmail.com>
2021-12-03 16:24:24 +0000

    Async functions for getting prices

---
+++ CryptoPrice/Currencies/Symbols.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoPrice.Currencies
+{
+    public static class Symbol
+    {
+        public static Dictionary<String, String> Get = new Dictionary<string, string>
+        {
+            //Europe
+            {Fiat.BGN,"лв "},
+            {Fiat.CHF,"CHF "},
+            {Fiat.CZK,"Kč "},
+            {Fiat.DKK,"kr "},
+            {Fiat.EUR,"€"},
+            {Fiat.GBP,"£"},
+            {Fiat.GEL,"₾"},
+            {Fiat.HUF,"ft "},
+            {Fiat.NOK,"kr "},
+            {Fiat.PLN,"zł "},
+            {Fiat.RUB,"₽"},
+            {Fiat.RON,"lei "},
+            {Fiat.SEK,"kr "},
+            {Fiat.TRY,"₺"},
+            {Fiat.UAH,"₴"},
+
+            //Middle east & Africa
+        };
+    }
+}
